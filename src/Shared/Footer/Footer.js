ed66752a30eadf2b1ef5561d5b2bf16cc32c0f22
DEWAN <dewanmahinchy@gmail.com>
2021-11-17 17:54:20 +0600

    footer ui done

--- src/Shared/Footer/Footer.js
+++ src/Shared/Footer/Footer.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Footer.css'
 
 const Footer = () => {
     return (
-        <div>
-            <h2>This is footer</h2>
+        <div className="footer">
+            <div className="footer1">
+                <div className="container">
+                    <div className="row align-items-center">
+                        <div className="col-lg-4 col-12">
+                            <p className="m-0">East Madison Street,<br /> Baltimore, MD, USA</p>
+                        </div>
+                        <div className="col-lg-4 col-12">
+                            <Link className="navbar-brand logo" to="/">Wrist<span style={{color: '#b29466', fontSize: '43px', fontFamily: 'auto'}}>K</span>ing</Link>
+                        </div>
+                        <div className="col-lg-4 col-12">
+                            <p className="m-0">info@example.com<br /> +000 - 123456789</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="footer2">
+                <p className="m-0">Copyright &copy; 2021 wristKing. Designed by <strong><a href="https://github.com/dewan36">Dewan Mahin Chowdhury</a></strong></p>
+            </div>
         </div>
     );
 };
